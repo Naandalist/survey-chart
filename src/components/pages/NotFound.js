38337b924249309c00de6710fa9749a71a58677b
Naandalist <listiananda.apriliawan@gmail.com>
2020-07-19 15:18:18 +0700

    chore: fix notFound

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Layout, Typography } from "antd";
+
+const { Content, Footer } = Layout;
+const { Title, Paragraph } = Typography;
 
 function NotFound() {
   return (
-    <div>
-      <h1>Tidak ditemukan apa-apa</h1>
-      <p className='lead'>Kamu tidak mendapatkan apa-apa di halaman ini</p>
-    </div>
+    <Fragment>
+      <Layout>
+        <Content
+          className='site-layout'
+          style={{ padding: "0 50px", marginTop: 40 }}
+        >
+          <div
+            className='site-layout-background'
+            style={{ padding: 24, minHeight: 500 }}
+          >
+            <Title level={2}>Tidak ditemukan apa-apa</Title>
+            <Paragraph>
+              Maaf, kita tidak mendapatkan apa-apa pada halaman ini
+            </Paragraph>
+            {/* Chart BEGIN */}
+
+            {/* Chart END */}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Sekolah Tinggi Ilmu Al-Qur'an (STIQ) Al-Lathifiyyah Palembang 2016 -
+          2020
+        </Footer>
+      </Layout>
+    </Fragment>
   );
 }
 
